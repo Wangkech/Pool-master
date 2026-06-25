a02e46d95c6e09598a6ab7461d0e9b64290995c3
Wangkech <wangkechgathothgatkuoth@gmail.com>
2026-06-25 10:21:07 +0300

    Settting default extensions to prevent lose upon reload in the stack blitz IDE

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+import "./css/App.css";
 
 function App() {
-  return <></>;
+  return (<>
+    
+  </>);
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+    return (
+        
+        <nav class="top-nav">
+      <div class="nav-content">
+        <div class="burger-btn">
+          <div class="burger-btn-line" id="burger-btn-line1"></div>
+          <div class="burger-btn-line" id="burger-btn-line2"></div>
+          <div class="burger-btn-line" id="burger-btn-line3"></div>
+        </div>
+        <div class="logo-banner"><h1 class="logo">Pool Master</h1></div>
+      </div>
+    </nav>
+        
+    )
+}
