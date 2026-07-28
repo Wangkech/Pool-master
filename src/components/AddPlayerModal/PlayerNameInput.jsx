8fd3a13d57a8a88cefaf878065fb8d0fd8b76c84
Wangkech <wangkechgathothgatkuoth@gmail.com>
2026-07-28 10:31:27 +0300

    Fixed styling on the playername Input

--- src/components/AddPlayerModal/PlayerNameInput.jsx
+++ src/components/AddPlayerModal/PlayerNameInput.jsx
@@ -5,19 +5,19 @@ function PlayerNameInput({ playerName, getPlayerName, addPlayerToList }) {
   return (
     <form
       action="submit"
-      className="add-player-form align-center flex justify-center gap-4"
+      className="add-player-form align-center grid max-w-125 grid-cols-[1fr_3rem] justify-center gap-2"
     >
       <input
         type="text"
-        className="player-name-input flex h-10 w-auto justify-center gap-2 rounded-[5rem] border-2 border-none bg-white p-4 text-[0.875rem] text-black shadow-black placeholder:text-[#1F1F1F]"
+        className="player-name-input flex h-10 min-w-[60%] justify-center gap-2 rounded-[5rem] border-2 border-none bg-white p-4 text-[0.875rem] text-black shadow-black placeholder:text-[#1F1F1F]"
         placeholder=" Enter Player Name...  "
         onChange={getPlayerName}
         value={playerName}
       />
       <Actionbtn
         id="add-player"
-        style="text-[12px] shadow-[0_0_4px_20px_rgb(20_33_23_0.5)]"
-        text="Add Player"
+        style="text-[12px] shadow-[0_0_4px_20px_rgb(20_33_23_0.5)] h-f"
+        text="Add"
         action={addPlayerToList}
         imgURL={""}
       />
