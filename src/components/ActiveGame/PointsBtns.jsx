72eef32b1085b54d0231b6aa8b47151ebe37a02b
Wangkech <wangkechgathothgatkuoth@gmail.com>
2026-08-15 22:31:08 +0300

    feature: added an alert when no ball available

--- src/components/ActiveGame/PointsBtns.jsx
+++ src/components/ActiveGame/PointsBtns.jsx
@@ -5,6 +5,7 @@ import {
   faTimes,
 } from "@fortawesome/free-solid-svg-icons";
 import { useGameContext } from "../../context/useGameContext";
+import useDialog from "../../context/useDialog";
 
 function PointsBtns({ isAdding, setIsAdding, isFouling, setIsFouling }) {
   const { availableBalls } = useGameContext();
@@ -32,44 +33,48 @@ function PointsBtns({ isAdding, setIsAdding, isFouling, setIsFouling }) {
       setIsFouling(true);
     }
   };
+  const { alert } = useDialog();
+
+  async function noBallsAlert() {
+    await alert({
+      title: "No Available Balls",
+      message: "All balls have been potted!",
+      type: "info",
+      onAction: [() => {}],
+      actionText: "OK",
+    });
+  }
+  if (availableBalls.length === 0) {
+    setIsFouling(false);
+    setIsFouling(false);
+  }
   return (
     <div className="points-btns w-ful bg-green-8d00 h-full rounded-t-2xl">
-      {isAdding ? (
-        <div className="flex h-full items-center rounded-t-2xl bg-green-800">
-          <FontAwesomeIcon
-            icon={faTimes}
-            onClick={handlePlusbtn}
-            className="points-btn relative cursor-pointer text-[#3f813f]"
-          />
-        </div>
-      ) : (
-        <div className="bg-green-none flex h-full items-center rounded-t-2xl">
-          <FontAwesomeIcon
-            icon={faPlusCircle}
-            onClick={handlePlusbtn}
-            className="points-btn relative cursor-pointer text-green-800"
-          />
-        </div>
-      )}
-      {isFouling ? (
-        <div className="z-10 flex h-full items-center rounded-t-2xl bg-red-950">
-          <FontAwesomeIcon
-            icon={faTimes}
-            onClick={handleMinusBtn}
-
-            className="minus-points-btn points-btn relative cursor-pointer text-[#a12626]"
-          />
-        </div>
-      ) : (
-        <div className="z-10 flex h-full items-center rounded-t-2xl bg-none">
-          <FontAwesomeIcon
-            icon={faMinusCircle}
-            onClick={handleMinusBtn}
-
-            className="minus-points-btn points-btn relative cursor-pointer text-[#fff]"
-          />
-        </div>
-      )}
+      <div
+        className={`bg-green-none flex h-full items-center rounded-t-2xl ${isAdding && "rounded-t-2xl bg-green-800 "}`}
+      >
+        <FontAwesomeIcon
+          onClick={() => {
+            if (availableBalls.length === 0) {
+              console.log(availableBalls);
+              noBallsAlert();
+            }
+            handlePlusbtn();
+          }}
+          icon={isAdding ? faTimes : faPlusCircle}
+          className={`points-btn relative cursor-pointer ${isAdding ? "text-[#3f813f]" : "text-green-800"}`}
+        />
+      </div>
+      <div
+        className={`z-10 flex h-full items-center bg-none ${isFouling && "rounded-t-2xl bg-red-950"}`}
+      >
+        <FontAwesomeIcon
+          icon={isFouling ? faTimes : faMinusCircle}
+          onClick={handleMinusBtn}
+          className={`minus-points-btn points-btn relative cursor-pointer ${isFouling ? " text-[#a12626]" : "text-white"}`}
+        />
+      </div>
+      {/* )} */}
     </div>
   );
 }
